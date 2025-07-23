0c3815810dc0bb6aaf51ad0efba56322c5ded68b
SebastianCorrea-dev <sebascor62@gmail.com>
2025-07-23 14:52:18 -0500

    Create the maze class to draw out the maze

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from graphics import Window
-from cell import Cell
+from maze import Maze
 
 def main():
-    win = Window(800, 600)
-    
-    c1 = Cell(win)
-    c1.draw(50,100,50,100)
-
-    c2 = Cell(win)
-    c2.draw(125,175,50,200)
-
-    c1.draw_move(c2)
+    num_rows = 20
+    num_cols = 30
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
 
     win.wait_for_close()
 main()
---
+++ maze.py
@@ -0,0 +1,42 @@
+from cell import Cell
+import time
+class Maze:
+    def __init__(
+            self,
+            x1,
+            y1,
+            num_rows,
+            num_cols,
+            cell_size_x,
+            cell_size_y,
+            win
+    ):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__num_rows = num_rows
+        self.__num_cols = num_cols
+        self.__cell_size_x = cell_size_x
+        self.__cell_size_y = cell_size_y
+        self.__win = win
+        self.__cells = []
+        self.__create_cells()
+
+    def __create_cells(self):
+        for col in range(0, self.__num_cols):
+            self.__cells.append([])
+            for row in range(0, self.__num_rows):
+                cell = Cell(self.__win)
+                self.__cells[col].append(cell)
+                self.__draw_cell(col, row)
+  
+    def __draw_cell(self, i, j):
+        x1 = self.__x1 + i * self.__cell_size_x
+        x2 = x1 + self.__cell_size_x
+        y1 = self.__y1 + j * self.__cell_size_y
+        y2 = y1 + self.__cell_size_y
+        self.__cells[i][j].draw(x1, x2, y1, y2)
+        self.__animate()
+    
+    def __animate(self):
+        self.__win.redraw()
+        time.sleep(0.05)
